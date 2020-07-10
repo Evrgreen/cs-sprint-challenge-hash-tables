6b01a7f66882d7aa3e5d8a6e7b87581c0c7e4934
evrgreen <rcarsten06@gmail.com>
2020-07-10 10:10:08 -0600

    completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,15 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_store = {}
+    for index, weight in enumerate(weights):
+        if weight not in weight_store:
+            weight_store[weight] = index
+    for item in range(length):
+
+        item_weight = weights[item]
+        second_weight = limit - item_weight
+        if second_weight in weight_store and weight_store[second_weight] != item:
+            return(max(item, weight_store[second_weight]), min(item, weight_store[second_weight]))
 
     return None
