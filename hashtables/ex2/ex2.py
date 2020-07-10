ce67d8bf2694ef72abb97f236da8e543982d3362
evrgreen <rcarsten06@gmail.com>
2020-07-10 11:13:15 -0600

    completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,24 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = [None] * length
+    flights = {}
+    for ticket in tickets:
+        if ticket.source == 'NONE':
+            route[0] = ticket.destination
+        flights[ticket.source] = ticket.destination
+
+    for index in range(1, length):
+        source = route[index-1]
+        route[index] = flights[source]
 
     return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
